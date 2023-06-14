5d2f928a4d86c446c07b0c8255b2c6ec245aca6f
israelsanttana <israelsantanasantos7@gmail.com>
2023-06-14 16:29:40 -0300

    feat: adiciona seleção de timer e options nos inputs

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -12,13 +12,28 @@ export function Home() {
                     <label htmlFor="task">Vou trabalhar em </label>
                     <TaskInput
                         id="task"
-                        placeholder="Nome da sua tarefa" />
+                        placeholder="Nome da sua tarefa"
+                        list="task-suggestions"
+                    />
+
+                    <datalist id="task-suggestions">
+                        <option value="1" />
+                        <option value="2" />
+                        <option value="3" />
+                        <option value="4" />
+                        <option value="5" />
+                    </datalist>
+
 
                     <label htmlFor="">Durante</label>
                     <MinutesAmountInput
                         type="number"
+                        step={5}
+                        min={5}
+                        max={60}
                         id="minutesAmount"
-                        placeholder="00" />
+                        placeholder="00"
+                    />
 
                     <span>minutos.</span>
 
